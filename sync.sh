2f496babcc618b012c89644ad7511fbc5d6e1ed2
lesteven <stevenle2011@gmail.com>
2021-06-06 13:10:01 -0700

    add sync script

---
+++ sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# matches every file in dir
+shopt -s dotglob
+
+for file in *; do
+    if [ "$file" != "sync.sh" ] && [ "$file" != ".git" ]; then
+        rm "/home/$USER/$file" -rf
+        ln -sv "$(pwd)/$file" ~ 
+    fi
+done
